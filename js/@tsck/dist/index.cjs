fd42e32cf80320e0def50bdfa52aad64ec735266
flipnism <jimikill3d@gmail.com>
2026-02-03 04:41:39 +0700

    remove shit

--- js/@tsck/dist/index.cjs
+++
@@ -1,4 +0,0 @@
-var{defineProperty:a,getOwnPropertyNames:i,getOwnPropertyDescriptor:p}=Object,u=Object.prototype.hasOwnProperty;var o=new WeakMap,s=(e)=>{var t=o.get(e),n;if(t)return t;if(t=a({},"__esModule",{value:!0}),e&&typeof e==="object"||typeof e==="function")i(e).map((r)=>!u.call(t,r)&&a(t,r,{get:()=>e[r],enumerable:!(n=p(e,r))||n.enumerable}));return o.set(e,t),t};var l={};module.exports=s(l);
-
-//# debugId=DD4114973F5A189564756E2164756E21
-//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAic291cmNlcyI6IFtdLAogICJzb3VyY2VzQ29udGVudCI6IFsKICBdLAogICJtYXBwaW5ncyI6ICIiLAogICJkZWJ1Z0lkIjogIkRENDExNDk3M0Y1QTE4OTU2NDc1NkUyMTY0NzU2RTIxIiwKICAibmFtZXMiOiBbXQp9
--- js/@tsck/dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from "./tsck.types";
-//# sourceMappingURL=index.d.ts.map
